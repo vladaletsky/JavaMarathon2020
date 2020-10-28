57b557e28e414d71f1852f383fc4f7831516e585
Vladislav Daletsky <44464499+vladaletsky@users.noreply.github.com>
2020-10-28 23:01:07 +0300

    day3 complete

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
 
+
+        }
     }
 }
